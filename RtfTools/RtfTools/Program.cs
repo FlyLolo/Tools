5e5b4862c4bffbc214cc4ec4fcca0301dcb70fd2
FlyLolo <flylolo@126.com>
2020-03-21 15:28:49 +0800

    rtf tools

---
+++ RtfTools/RtfTools/Program.cs
@@ -0,0 +1,41 @@
+﻿using Spire.Doc;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RtfTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                Console.WriteLine("args error");
+            }
+
+            Rtf2Image(args);
+        }
+
+        static void Rtf2Image(string[] args)
+        {
+            if (!Directory.Exists(args[0]))
+            {
+                Directory.CreateDirectory(args[0]);
+            }
+
+            Document document = new Document();
+            document.LoadRtf(args[1],Encoding.UTF8);
+            Image[] imgs = document.SaveToImages(Spire.Doc.Documents.ImageType.Bitmap);
+            foreach (var item in imgs)
+            {
+                imgs[0].Save(Path.Combine(args[0], Guid.NewGuid().ToString("N") + ".png"), ImageFormat.Png);
+            }
+        }
+    }
+}
